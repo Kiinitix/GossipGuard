b7e268942431233d9be17054cbed9c6b1623cc76
Kabir Dhruw <34811605+Kiinitix@users.noreply.github.com>
2026-04-21 00:24:39 +0530

    added SlidingWindow struct, fn new and fn increment

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use crdt::GCounter;
+use std::time::{Instant, Duration};
+
+#[derive(Debug, Clone)]
+pub struct SlidingWindow{
+    current: GCounter,
+    previous: GCounter,
+    window_start: Instant,
+    window_size: Duration,
+
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl SlidingWindow {
+    pub fn new(node_id: String, timeout: Duration) -> Self{
+        let current = GCounter::new(node_id.clone());
+        let previous = GCounter::new(node_id);
+        let window_start = Instant::now();
+        let window_size = timeout;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Self {
+            current, previous, window_start, window_size
+        }
+
     }
+
+    pub fn increment(&mut self) {
+    if self.window_start.elapsed() >= self.window_size {
+        let node_id = self.current.node_id.clone();
+        let old_current = std::mem::replace(
+            &mut self.current,
+            GCounter::new(node_id),
+        );
+        self.previous = old_current;
+        self.window_start = Instant::now();
+    }
+    self.current.increment();
 }
